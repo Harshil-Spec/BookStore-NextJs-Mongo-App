a12049347c2e02c50c1108b2330e5d23f79f5d1d
Harshil Joshi <harshil.joshi>
2025-03-25 15:19:05 +0530

    changes on config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      NEXT_PUBLIC_MONGO_URI: process.env.MONGO_URI,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
+    async headers() {
+      return [
+        {
+          source: "/(.*)",
+          headers: [
+            { key: "Referrer-Policy", value: "no-referrer" }, // or change to 'no-referrer'
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
